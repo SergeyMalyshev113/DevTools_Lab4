30205ba40028ae75b8856365be071af889028597
Aik <xaikq@mail.ru>
2022-11-16 21:49:17 +0300

    initial random func

---
+++ keyboard.py
@@ -0,0 +1,6 @@
+from aiogram import types
+
+mem = types.KeyboardButton('Вкинь мем')
+monetka = types.KeyboardButton('Подрось монетку')
+random = types.KeyboardButton('Рандомное число')
+back = types.KeyboardButton('Вернутсья назад')
